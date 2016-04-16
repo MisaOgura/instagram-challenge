c8a3cbbdf01e9610d316580587fbade18bd37b15
Misa Ogura <misa.ogura01@gmail.com>
2016-04-16 11:42:34 +0100

    Once a boards is added, users can see the link to the board in boards_path

--- spec/features/boards_features_spec.rb
+++ spec/features/boards_features_spec.rb
@@ -8,4 +8,14 @@ feature 'Boards:' do
       expect(page).to have_link 'Create new board'
     end
   end
+
+  context 'A board has been added' do
+    before { Board.create(name: 'Board 1') }
+
+    scenario 'displays a link to an existing board' do
+      visit boards_path
+      expect(page).not_to have_content 'No board has been added'
+      expect(page).to have_link 'Board 1'
+    end
+  end
 end
